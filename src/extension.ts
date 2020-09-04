c9839f5cd87a3d9d492a025524b07c0ea17edf7d
Peter Coulton <petercoulton@gmail.com>
2020-09-04 13:47:34 +0100

    feat: Generate hex ObjectIds

--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,38 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-import { v4 as uuidv4 } from 'uuid'
+import { v4 as uuidv4 } from 'uuid';
+import { ObjectId, serialize } from 'bson';
+
+function injectTextAtCursor(builder: vscode.TextEditorEdit, activeTextEditor: vscode.TextEditor | undefined, text: string) {
+	const caretPosition = activeTextEditor?.selection?.active;
+
+	if (caretPosition !== undefined) {
+		builder.insert(caretPosition, `${text}`);
+	}
+}
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-	let disposable = vscode.commands.registerCommand('theid.newUuid', () => {
-
+	context.subscriptions.push(vscode.commands.registerCommand('theid.newUuid', () => {
 		vscode.window.activeTextEditor?.edit((builder) => {
+			
+			const id = uuidv4();
 
-			const caretPosition = vscode.window.activeTextEditor?.selection?.active
+			injectTextAtCursor(builder, vscode.window.activeTextEditor, `${id}`);
+		});
+	}));
 
-			if (caretPosition !== undefined) {
-				builder.insert(caretPosition, `${uuidv4()}`)
-			}
-		})
-	});
+	context.subscriptions.push(vscode.commands.registerCommand('theid.newObjectId', () => {
+		vscode.window.activeTextEditor?.edit((builder) => {
+			
+			const id = ObjectId.generate().toString('hex');
 
-	context.subscriptions.push(disposable);
+			injectTextAtCursor(builder, vscode.window.activeTextEditor, `${id}`);
+		});
+	}));
 }
 
 // this method is called when your extension is deactivated
