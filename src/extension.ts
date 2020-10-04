e35f18d76d6ddc667c1fbd7d0e37de7641c406b7
Andrew Hatch <andrew@andrewhatch.net>
2020-10-04 22:07:39 +0100

    support more generators Added v1 as well as v4. Added Epoch (seconds) and Epoch (milliseconds). Made it easier to add more 1-line generator functions

--- src/extension.ts
+++ src/extension.ts
@@ -1,4 +1,5 @@
 import * as vscode from 'vscode';
+import { v1 as uuidv1 } from 'uuid';
 import { v4 as uuidv4 } from 'uuid';
 import { ObjectId } from 'bson';
 
@@ -10,27 +11,37 @@ function injectTextAtCursor(builder: vscode.TextEditorEdit, activeTextEditor: vs
 	}
 }
 
+function registerGeneratorFunction(
+	commandId: string,
+	generator: () => string,
+	context: vscode.ExtensionContext
+) {
+	context.subscriptions.push(vscode.commands.registerCommand(commandId, () => {
+		vscode.window.activeTextEditor?.edit((builder) => {
 
+			const id = generator();
+
+			injectTextAtCursor(builder, vscode.window.activeTextEditor, `${id}`);
+		});
+	}));
+}
+
+function registerIdGenerators(context: vscode.ExtensionContext) {
+	const commandFunctions = {
+		'theid.newUuidV1': () => uuidv1(),
+		'theid.newUuidV4': () => uuidv4(),
+		'theid.newObjectId': () => ObjectId.generate().toString('hex'),
+		'theid.newEpochId': () => Math.floor(+new Date() / 1000).toString(),
+		'theid.newEpochMillisId': () => Math.floor(+new Date()).toString()
+	}
+
+	for (const [commandId, uuidFunction] of Object.entries(commandFunctions)) {
+		registerGeneratorFunction(commandId, uuidFunction, context);
+	}
+}
 
 export function activate(context: vscode.ExtensionContext) {
-
-	context.subscriptions.push(vscode.commands.registerCommand('theid.newUuid', () => {
-		vscode.window.activeTextEditor?.edit((builder) => {
-			
-			const id = uuidv4();
-
-			injectTextAtCursor(builder, vscode.window.activeTextEditor, `${id}`);
-		});
-	}));
-
-	context.subscriptions.push(vscode.commands.registerCommand('theid.newObjectId', () => {
-		vscode.window.activeTextEditor?.edit((builder) => {
-			
-			const id = ObjectId.generate().toString('hex');
-
-			injectTextAtCursor(builder, vscode.window.activeTextEditor, `${id}`);
-		});
-	}));
+	registerIdGenerators(context);
 }
 
 export function deactivate() { }
